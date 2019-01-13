8d565e309ca9e97444d24aedcb606575d27519a5
Jal Panchal <jal08panchal@gmail.com>
2019-01-13 12:29:32 -0800

    Hiding the dice and setting scores to zero

--- app.js
+++ app.js
@@ -17,4 +17,9 @@ activePlayer = 0;
 
 dice = Math.floor(Math.random()*6) + 1;
 
+document.querySelector('.dice').style.display = 'none';
 
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
